296bed86b2d844bf80d78ec1134eece86fd1c114
Kyle Osterman <57078519+kylejosterman@users.noreply.github.com>
2021-03-27 10:30:11 -0400

    Initial Commit

---
+++ Park Finder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Park Finder
+//
+//  Created by Kyle Osterman on 3/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
